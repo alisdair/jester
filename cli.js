65fd19700d279d17774d1cb4d1b498fb6cf0a04d
Alisdair McDiarmid <alisdair@mcdiarmid.org>
2013-10-02 21:11:14 +0100

    Stub out a REPL to help understand eval problems.

--- cli.js
+++ cli.js
@@ -3,13 +3,15 @@
 var jester  = require('./'),
     fs      = require('fs');
 
+var context = jester.createContext();
+context.start();
+
 var args = process.argv.slice(1);
 
-fs.readFile(args[1], 'utf8', function(err, source) {
-
-    var context = jester.createContext();
-
-    context.start();
-    context.run(source, args[0]);
-
-});
+if (args.length < 2) {
+    context.repl();
+} else {
+    fs.readFile(args[1], 'utf8', function(err, source) {
+        context.run(source, args[0]);
+    });
+}
